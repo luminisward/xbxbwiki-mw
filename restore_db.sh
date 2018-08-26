03ca5df3e101f850c59b3afe6c136a6143411024
monado <monado@xenoblade.cn>
2018-08-26 23:09:44 +0800

    auto detect dbname

--- restore_db.sh
+++ restore_db.sh
@@ -1,8 +1,9 @@
+DBname=`grep '$wgDBname' mediawiki/LocalSettings.php | awk -F "[\"\"]" '{print $2}'`
 DBuser=`grep '$wgDBuser' mediawiki/LocalSettings.php | awk -F "[\"\"]" '{print $2}'`
 DBpassword=`grep '$wgDBpassword' mediawiki/LocalSettings.php | awk -F "[\"\"]" '{print $2}'`
 
 MYSQL_CONTAINER=`docker ps | grep mysql | awk '{print $NF}'`
 
-docker exec $MYSQL_CONTAINER mysql -u$DBuser -p$DBpassword -e 'DROP DATABASE IF EXISTS xbxbwiki;'
-docker exec $MYSQL_CONTAINER mysql -u$DBuser -p$DBpassword -e 'CREATE DATABASE xbxbwiki;'
-docker exec -i $MYSQL_CONTAINER mysql -u$DBuser -p$DBpassword xbxbwiki < $1
+docker exec $MYSQL_CONTAINER mysql -u$DBuser -p$DBpassword -e "DROP DATABASE IF EXISTS $DBname;"
+docker exec $MYSQL_CONTAINER mysql -u$DBuser -p$DBpassword -e "CREATE DATABASE $DBname;"
+docker exec -i $MYSQL_CONTAINER mysql -u$DBuser -p$DBpassword $DBname < $1
