3e39fda166e5e10d60c97c28152a3fde013ecc6a
monado <monado@xenoblade.cn>
2018-08-26 22:12:52 +0800

    update backup scripts

--- backup_db.sh
+++ backup_db.sh
@@ -1,1 +1,3 @@
-docker-compose exec mysql mysqldump -uroot -pMjqVXTUhDgp4xsakiwT5 xbxbwiki > xbxbwiki.sql
+DBuser=`grep '$wgDBuser' mediawiki/LocalSettings.php | awk -F "[\"\"]" '{print $2}'`
+DBpassword=`grep '$wgDBpassword' mediawiki/LocalSettings.php | awk -F "[\"\"]" '{print $2}'`
+docker-compose exec mysql mysqldump -u $DBuser -p $DBpassword xbxbwiki > xbxbwiki`date +%y%m%d_%H%M%S`.sql
--- restore_db.sh
+++ restore_db.sh
@@ -1,5 +1,8 @@
+DBuser=`grep '$wgDBuser' mediawiki/LocalSettings.php | awk -F "[\"\"]" '{print $2}'`
+DBpassword=`grep '$wgDBpassword' mediawiki/LocalSettings.php | awk -F "[\"\"]" '{print $2}'`
+
 MYSQL_CONTAINER=`docker ps | grep mysql | awk '{print $NF}'`
 
-docker exec $MYSQL_CONTAINER mysql -uroot -pMjqVXTUhDgp4xsakiwT5 -e 'DROP DATABASE IF EXISTS xbxbwiki;' > /dev/null 2>&1
-docker exec $MYSQL_CONTAINER mysql -uroot -pMjqVXTUhDgp4xsakiwT5 -e 'CREATE DATABASE xbxbwiki;' > /dev/null 2>&1
-docker exec -i $MYSQL_CONTAINER mysql -uroot -pMjqVXTUhDgp4xsakiwT5 xbxbwiki < xbxbwiki.sql > /dev/null 2>&1
+docker exec $MYSQL_CONTAINER mysql -u $DBuser -p $DBpassword -e 'DROP DATABASE IF EXISTS xbxbwiki;'
+docker exec $MYSQL_CONTAINER mysql -u $DBuser -p $DBpassword -e 'CREATE DATABASE xbxbwiki;'
+docker exec -i $MYSQL_CONTAINER mysql -u $DBuser -p $DBpassword xbxbwiki < $1
