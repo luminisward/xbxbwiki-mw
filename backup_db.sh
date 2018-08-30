7a414571304901066f33d8d5f525f7ac5775d6fb
monado <monado@xenoblade.cn>
2018-08-30 15:57:08 +0800

    lock db when mysqldump

--- backup_db.sh
+++ backup_db.sh
@@ -4,6 +4,8 @@ DBpassword=`grep '$wgDBpassword' mediawiki/LocalSettings.php | awk -F "[\"\"]" '
 
 backupFile="xbxbwiki`date +%y%m%d_%H%M%S`.sql"
 
+echo '$wgReadOnly = "'"Dumping Database, Access will be restored shortly"'";' >> mediawiki/LocalSettings.php
 docker-compose exec mysql mysqldump -u$DBuser -p$DBpassword $DBname > $backupFile
+sed -i "/wgReadOnly/d" mediawiki/LocalSettings.php
 sed -i "/mysqldump/d" $backupFile
 gzip $backupFile
