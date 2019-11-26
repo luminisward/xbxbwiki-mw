e2e122a71372e1cb0121e24310a4edaec8356539
monado <monado@xenoblade.cn>
2019-11-27 01:55:31 +0800

    not throw connect error on construct

--- mediawiki/extensions/XBDBScribunto/XBDBScribuntoLuaLibrary.php
+++ mediawiki/extensions/XBDBScribunto/XBDBScribuntoLuaLibrary.php
@@ -4,6 +4,7 @@ class XBDBScribuntoLuaLibrary extends Scribunto_LuaLibraryBase
 {
     private $conn;
     private $tables;
+    private $conn_error;
 
     public function __construct(Scribunto_LuaEngine $engine)
     {
@@ -17,9 +18,13 @@ class XBDBScribuntoLuaLibrary extends Scribunto_LuaLibraryBase
 
         $e = function_exists(pg_connect);
 
-        $this->conn = new PDO("pgsql:host=$servername;dbname=$dbname;user=$username;password=$password");
-        if (!$this->conn) {
-            throw new Exception("连接失败: " . $this->conn->connect_error);
+        try {
+            $this->conn = new PDO("pgsql:host=$servername;dbname=$dbname;user=$username;password=$password");
+        } catch (\Throwable $th) {
+            $this->conn_error = $th;
+        }
+        if ($this->conn->connect_error) {
+            $this->conn_error = new Exception("连接失败: " . $this->conn->connect_error);
         }
     }
 
@@ -38,6 +43,10 @@ class XBDBScribuntoLuaLibrary extends Scribunto_LuaLibraryBase
 
     public function query($sql, $schema)
     {
+        if ($this->conn_error) {
+            throw $this->conn_error;
+        }
+
         $this->conn->exec("SET search_path TO $schema");
         $stocks = [];
         if ($stmt = $this->conn->query($sql)) {
