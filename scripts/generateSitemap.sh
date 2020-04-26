3cc7f6c6384522ebc2aa1b5d3e65d6b7ae1d3d73
luminisward <luminis@vip.qq.com>
2020-04-27 05:03:33 +0800

    update scripts

--- scripts/generateSitemap.sh
+++ scripts/generateSitemap.sh
@@ -1,1 +1,2 @@
-docker-compose exec xbxb_phpfpm php maintenance/generateSitemap.php --urlpath http://xenoblade2.cn --compress no
+docker-compose exec xbxb_phpfpm php maintenance/generateSitemap.php --urlpath https://xenoblade2.cn --compress no
+docker-compose exec xbd_phpfpm php maintenance/generateSitemap.php --urlpath https://xenoblade.cn --compress no
