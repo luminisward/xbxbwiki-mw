abf245d3427f308f245dc9c8a8281902d91de990
luminisward <luminis@vip.qq.com>
2020-04-29 02:51:51 +0800

    update scripts

--- scripts/rebuildData.sh
+++ scripts/rebuildData.sh
@@ -1,1 +1,2 @@
 docker-compose exec xbxb_phpfpm php extensions/SemanticMediaWiki/maintenance/rebuildData.php -v
+docker-compose exec xbd_phpfpm php extensions/SemanticMediaWiki/maintenance/rebuildData.php -v
---
+++ scripts/restart-php.sh
@@ -0,0 +1,1 @@
+docker-compose restart xbxb_nginx xbxb_phpfpm xbd_nginx xbd_phpfpm
